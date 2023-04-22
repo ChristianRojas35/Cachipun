d0741df7182bc3fc39ea94ea8ddc381b348f4f5b
Christian <christianrojas.palomino@gmail.com>
2023-04-22 12:55:26 -0400

    Correcion de errores y cambios importantes.

--- assets/js/logica.js
+++ assets/js/logica.js
@@ -3,11 +3,12 @@ let jugadaUsuario
 let jugadaComputadora
 let jugadaComparativa
 
+
 cantidadPartidas = parseInt(prompt("Ingresa cantidad de partidas"));
 
 for(let i = 0 ; i < cantidadPartidas ; i++) {
+    console.log("Ciclo: " + i);
     jugadaUsuario = prompt("Ingresa tu jugada: Piedra, Papel o Tijeras");
-
     switch (jugadaUsuario.toLocaleLowerCase()) {
         case "piedra":
             jugadaComparativa = 0;
@@ -15,7 +16,7 @@ for(let i = 0 ; i < cantidadPartidas ; i++) {
         case  "papel":
             jugadaComparativa = 1;
             break;
-        case "tijera":
+        case "tijera", "tijeras":
             jugadaComparativa = 2;
             break;
         default:
@@ -26,26 +27,31 @@ for(let i = 0 ; i < cantidadPartidas ; i++) {
     jugadaComputadora = Math.floor(Math.random()*3);
 
     if(jugadaComparativa == 3) {
+        console.log("jugadaComputadora: " + jugadaComputadora);
+        console.log("jugadaComparativa: " + jugadaComparativa);
         console.log("Opcion ingresada invalida");
     }
     else if((jugadaComputadora == 0 && jugadaComparativa == 0) || (jugadaComputadora == 1 && jugadaComparativa == 1) || (jugadaComputadora == 2 && jugadaComparativa == 2))
     {
-        console.log("Esto es un empate");
+        
+        alert("Esto es un empate");
     }
     //Computadora gana
     else if((jugadaComputadora == 0 && jugadaComparativa == 1) ||
      (jugadaComputadora == 1 && jugadaComparativa == 2) || 
      (jugadaComputadora == 2 && jugadaComparativa == 0))
      {
-        console.log("Computadora gana");
+        console.log("jugadaComputadora: " + jugadaComputadora);
+        console.log("jugadaComparativa: " + jugadaComparativa);
+        alert("Computadora gana");
      }
      //Usuario gana
      else if((jugadaComputadora == 0 && jugadaComparativa == 2) ||
      (jugadaComputadora == 1 && jugadaComparativa == 0) || 
      (jugadaComputadora == 2 && jugadaComparativa == 1))
      {
-        console.log("Usuario gana");
+        console.log("jugadaComputadora: " + jugadaComputadora);
+        console.log("jugadaComparativa: " + jugadaComparativa);
+        alert("Usuario gana juego");
      }
-
-
 }
