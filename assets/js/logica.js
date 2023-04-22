e8cb9a1243a36aee0bc332aaeb6a88bac90fd238
Christian <christianrojas.palomino@gmail.com>
2023-04-22 14:03:16 -0400

    Correcciones

--- assets/js/logica.js
+++ assets/js/logica.js
@@ -1,57 +1,91 @@
-let cantidadPartidas
-let jugadaUsuario
-let jugadaComputadora
-let jugadaComparativa
+//Variables
+let cantidadJuegos; //define el numero de juegos por partida.
+let opcionUsuario; //define la elección del usuario (Piedra, Papel, Tijera) en formato texto.
+let opcionComputadora;//define la opción random de la computadora.
+let jugadaUsuario;//define la elección del usuario convertida a entero.
+let jugadaComputadora;
+
+let jugadasGanadasUsuario;//define 
+let jugadasGanadasComputadora;
 
 
-cantidadPartidas = parseInt(prompt("Ingresa cantidad de partidas"));
+cantidadJuegos = parseInt(prompt("Ingresa cantidad de partidas"));
 
-for(let i = 0 ; i < cantidadPartidas ; i++) {
+for(let i = 0 ; i < cantidadJuegos ; i++) {
     console.log("Ciclo: " + i);
-    jugadaUsuario = prompt("Ingresa tu jugada: Piedra, Papel o Tijeras");
-    switch (jugadaUsuario.toLocaleLowerCase()) {
+    opcionUsuario = prompt("Ingresa tu jugada: Piedra, Papel o Tijeras");
+    console.log("opcionUsuario desde el prompt: " + opcionUsuario);
+    switch (opcionUsuario.toLocaleLowerCase()) {
         case "piedra":
-            jugadaComparativa = 0;
+            jugadaUsuario = 0;
             break;
         case  "papel":
-            jugadaComparativa = 1;
+            jugadaUsuario = 1;
             break;
         case "tijera", "tijeras":
-            jugadaComparativa = 2;
+            jugadaUsuario = 2;
             break;
-        default:
-            jugadaComparativa = 3;
+        case "lunes":
+            jugadaUsuario = 3;
             break;
     }
 
     jugadaComputadora = Math.floor(Math.random()*3);
 
-    if(jugadaComparativa == 3) {
-        console.log("jugadaComputadora: " + jugadaComputadora);
-        console.log("jugadaComparativa: " + jugadaComparativa);
-        console.log("Opcion ingresada invalida");
-    }
-    else if((jugadaComputadora == 0 && jugadaComparativa == 0) || (jugadaComputadora == 1 && jugadaComparativa == 1) || (jugadaComputadora == 2 && jugadaComparativa == 2))
+    switch (jugadaComputadora)
     {
-        
-        alert("Esto es un empate");
+        case 0:
+            opcionComputadora = "Piedra"
+            break;
+        case 1:
+            opcionComputadora = "Papel"
+            break;
+        case 2:
+            opcionComputadora = "Tijera"
+            break;
+
+    }
+
+    if(jugadaUsuario === 3) {
+        alert("Opcion ingresada invalida");
+    }
+    else if((jugadaComputadora == 0 && jugadaUsuario == 0) || (jugadaComputadora == 1 && jugadaUsuario == 1) || (jugadaComputadora == 2 && jugadaUsuario == 2))
+    {
+        console.log("opcionComputadora: " + opcionComputadora);
+        console.log("opcionUsuario: " + opcionUsuario);
+        alert("Este juego es un empate");
     }
     //Computadora gana
-    else if((jugadaComputadora == 0 && jugadaComparativa == 1) ||
-     (jugadaComputadora == 1 && jugadaComparativa == 2) || 
-     (jugadaComputadora == 2 && jugadaComparativa == 0))
+    else if((jugadaComputadora == 0 && jugadaUsuario == 2) ||
+     (jugadaComputadora == 1 && jugadaUsuario == 0) || 
+     (jugadaComputadora == 2 && jugadaUsuario == 1))
      {
-        console.log("jugadaComputadora: " + jugadaComputadora);
-        console.log("jugadaComparativa: " + jugadaComparativa);
-        alert("Computadora gana");
+        console.log("opcionComputadora: " + opcionComputadora);
+        console.log("opcionUsuario: " + opcionUsuario);
+        jugadasGanadasComputadora ++;
+        alert("Este juego lo ha ganado la computadora");
      }
      //Usuario gana
-     else if((jugadaComputadora == 0 && jugadaComparativa == 2) ||
-     (jugadaComputadora == 1 && jugadaComparativa == 0) || 
-     (jugadaComputadora == 2 && jugadaComparativa == 1))
+     else if((jugadaComputadora == 0 && jugadaUsuario == 1) ||
+     (jugadaComputadora == 1 && jugadaUsuario == 2) || 
+     (jugadaComputadora == 2 && jugadaUsuario == 0))
      {
-        console.log("jugadaComputadora: " + jugadaComputadora);
-        console.log("jugadaComparativa: " + jugadaComparativa);
-        alert("Usuario gana juego");
+        console.log("opcionComputadora: " + opcionComputadora);
+        console.log("opcionUsuario: " + opcionUsuario);
+        jugadasGanadasUsuario ++;
+        alert("Este juego lo ha ganado el usuario");
      }
+}
+
+if(jugadasGanadasUsuario > jugadasGanadasComputadora) {
+    alert("El usuario ha ganado la partida");
+}
+else if (jugadasGanadasUsuario < jugadasGanadasComputadora) {
+    alert("La computadora ha ganado la partida HAHA");
+}
+else if (jugadasGanadasUsuario == jugadasGanadasComputadora) {
+    alert("Esta partida ha sido un empate u.u");
+}
+else  {
+    console.log("jugadaUsuario: " + jugadaUsuario);
 }
